f690c86b086c10412ce33171755f8d4b6983cd5f
Jelson Araújo <jelsonmatheus200f@gmail.com>
2023-05-27 22:02:38 -0300

    refactor prints

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from lexicon import Lexicon
 def main():
     print('********** TOKENS: ******************\n')
 
+    
     arquivo = 'tests/6 - todos.txt'
     lexicon = Lexicon(arquivo)
+
+    print(f'{"TOKEN":<25} {"TIPO":<25}')
+    print(f'{"-"*20:<25} {"-"*20:<25}')
     
     token = lexicon.next_token()
     while token:
-        print(token)
+        print(f'{token.lexeme:<25} {token.TokenType.name:<25}')
         token = lexicon.next_token()
     print()
 
